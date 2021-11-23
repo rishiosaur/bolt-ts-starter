a640612ab363b72489cfd40f9e18156c97db1688
Rishi Kothari <itsrishikothari@gmail.com>
2021-11-23 15:39:06 -0500

    style(style): :art: formatting

--- src/features/index.ts
+++ src/features/index.ts
@@ -1,3 +1,3 @@
 // This is where you should export all the features. Every folder in the starter kit follows this spec.
 
-export { default as feature1 } from './feature1';
+export { default as feature1 } from './feature1'
